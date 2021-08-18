ab0f933b03835b0ab0447776c82d0539d3f04769
climb719 <mary.oldweiler@gmail.com>
2021-08-18 16:27:13 -0400

    commit tree 2ea1184f6aec64cf96e6f8f2bac32227485a50ab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 100) + 1
+const num3 = 25
+const num4 = 7
+const mod = num3 % num4
+const max = Math.max(10, 11, 12, 13, 15, 17, 20);
